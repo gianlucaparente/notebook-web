1c1da57f89e321bd550387f912632a8242440455
Gianluca Parente <gianluca.parente@objectway.it>
2018-02-19 18:32:09 +0100

    create components folder and split into the vue components

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,41 +1,15 @@
 define([
     'vue',
     'axios',
-    'configuration'
-], function (vue, axios, configuration) {
+    './components/note-list/note-list.component',
+    './components/note-item/note-item.component'
+], function (vue, axios, NoteList, NoteItem) {
+
+    vue.component('note-list', NoteList);
+    vue.component('note-item', NoteItem);
 
     axios.get(`http://localhost:8080/notes`)
         .then(response => {
-
-            vue.component('note-list', {
-                props: ['notes', 'expired'],
-                template: "<table border='1' cellpadding='5' cellspacing='5'>" +
-                "<note-item v-for='note in filterNoteList(expired)' v-bind:note='note' v-bind:key='note.id'></note-item>" +
-                "</table>",
-                methods: {
-                    filterNoteList: function(expired) {
-
-                        if (expired === undefined) {
-                            return this.notes;
-                        }
-
-                        return this.notes.filter(function (note) {
-                            return note.expired === expired;
-                        });
-                    }
-                }
-            });
-
-            vue.component('note-item', {
-                props: ['note'],
-                template: '<tr>' +
-                '<td>{{ new Date(note.date).toLocaleDateString() }}</td>' +
-                '<td>{{ note.title }}</td>' +
-                '<td>{{ note.contact.name }}</td>' +
-                '<td>{{ note.contact.telephone1 }} - {{ note.contact.telephone2 }}</td>' +
-                '<td>{{ note.address }}</td>' +
-                '</tr>'
-            });
 
             var app = new vue({
                 el: '#app',
